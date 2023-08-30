326096fc72aa48eef3796e635aaa0cf8cae0b6f9
Lucas Piergiacomi <lucas@gestioncervecera.com>
2023-08-29 22:32:39 -0300

    agregado adminlte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { SettingsComponent } from './settings/settings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent,
+    SettingsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
