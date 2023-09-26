03bba62bfa220a25b7946eb3664503db5893df8b
Lucas Piergiacomi <lucas@gestioncervecera.com>
2023-09-26 14:43:32 -0300

    mejorado maquetado de dialogs + ordenamiento en tabla de clientes

--- src/app/pages/clientes/listado/listado.component.ts
+++ src/app/pages/clientes/listado/listado.component.ts
@@ -1,19 +1,20 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {Cliente } from "../../../backend/model/cliente";
-import {ClienteService} from "../../../backend/services/cliente.service";
-import {DialogCrearClienteComponent} from "../../lotes/dialog-crear-cliente/dialog-crear-cliente.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MatTable} from "@angular/material/table";
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Cliente } from "../../../backend/model/cliente";
+import { ClienteService } from "../../../backend/services/cliente.service";
+import { DialogCrearClienteComponent } from "../../lotes/dialog-crear-cliente/dialog-crear-cliente.component";
+import { MatDialog } from "@angular/material/dialog";
+import {MatTableDataSource} from "@angular/material/table";
+import { MatSort } from "@angular/material/sort";
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent implements OnInit{
-  clientes: Cliente[] = [];
-  displayedColumns: string[] = ['nombre', 'dni', 'email', 'telefono', 'direccion'];
-  @ViewChild(MatTable) table: MatTable<Cliente>;
+export class ListadoComponent implements OnInit {
+  dataSource: MatTableDataSource<Cliente>;
+  displayedColumns: string[] = ['nombre', 'documento', 'email', 'telefono', 'direccion'];
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private clienteService: ClienteService, private crearClienteDialog: MatDialog) {
   }
@@ -25,7 +26,10 @@ export class ListadoComponent implements OnInit{
   getClientes() {
     this.clienteService.getClientes().subscribe({
       next: (response) => {
-        this.clientes = response as Cliente[];
+        this.dataSource = new MatTableDataSource(response as Cliente[]);
+      },
+      complete: () => {
+        this.dataSource.sort = this.sort;
       },
       error: (error) => {
         console.error(error);
@@ -33,15 +37,16 @@ export class ListadoComponent implements OnInit{
     });
   }
 
-  test() {
+  abrirDialogCreacionCliente() {
     const dialogCrearCliente = this.crearClienteDialog.open(DialogCrearClienteComponent);
 
     dialogCrearCliente.afterClosed().subscribe((nuevoCliente: Cliente) => {
+
       if (nuevoCliente) {
-        console.log(nuevoCliente);
-        this.clientes.push(nuevoCliente);
-        this.table.renderRows();
+        this.dataSource.data.push(nuevoCliente);
+        this.dataSource.data = this.dataSource.data;
       }
+
     });
   }
 }
