ecb353566357e3104465f377de4a9753102894d2
Lucas Piergiacomi <lucas@gestioncervecera.com>
2023-10-06 15:25:14 -0300

    cambio en interceptor para que haga logout al recibir 401

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,28 +1,39 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
-  HttpRequest,
+  HttpRequest, HttpResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
+import {Observable, tap} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {AuthService} from "../backend/services/auth.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptor implements HttpInterceptor {
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+
+  constructor(private authService: AuthService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('access-token');
     if (token) {
-      const cloned = req.clone({
+      req = req.clone({
         headers: req.headers.set('Authorization', 'Bearer ' + token),
       });
-      return next.handle(cloned);
-    } else {
-      return next.handle(req);
     }
+    return next.handle(req).pipe(
+      tap({
+        error: (error) => {
+          if (error instanceof HttpErrorResponse) {
+            if (error.status === 401) {
+              this.authService.logout();
+            }
+          }
+        }
+      })
+    );
   }
 }
