6a3909f811aaec63780added2dc13487b1c3eee2
Lucas Piergiacomi <lg.piergiacomi@gmail.com>
2023-10-24 19:34:07 -0300

    test de limpiar filtros en mapa

--- src/app/pages/lots/map-lot-searcher/map-lot-searcher.component.spec.ts
+++ src/app/pages/lots/map-lot-searcher/map-lot-searcher.component.spec.ts
@@ -1,6 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapLotSearcherComponent } from './map-lot-searcher.component';
+import {LotsApi} from "../../../backend/api/lots-api.service";
+import {HttpService} from "../../../backend/api/http.service";
+import {HttpClientModule} from "@angular/common/http";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatInputModule} from "@angular/material/input";
+import {MatOptionModule} from "@angular/material/core";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('MapLotSearcherComponent', () => {
   let component: MapLotSearcherComponent;
@@ -8,7 +20,26 @@ describe('MapLotSearcherComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MapLotSearcherComponent]
+      declarations: [MapLotSearcherComponent],
+      imports: [
+        HttpClientModule,
+        MatDialogModule,
+        MatProgressSpinnerModule,
+        MatInputModule,
+        MatOptionModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatButtonModule,
+        MatExpansionModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+
+      ],
+      providers: [
+        LotsApi,
+        HttpService,
+
+      ]
     });
     fixture = TestBed.createComponent(MapLotSearcherComponent);
     component = fixture.componentInstance;
@@ -18,4 +49,33 @@ describe('MapLotSearcherComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display and update input value', () => {
+
+    const filterText = getByTestId('filterText');
+    const filterStates = getByTestId('filterStates');
+
+    filterText.value = 'Lote 1';
+    filterStates.value = ['DISPONIBLE', 'RESERVADO'];
+    fixture.detectChanges();
+
+    expect(filterText.value).toBe('Lote 1');
+    expect(filterStates.value).toEqual(['DISPONIBLE', 'RESERVADO']);
+
+    const filterLotsSpy = spyOn(component, 'filterLots');
+    getByTestId('btnCleanFilters').click();
+    fixture.detectChanges();
+
+    expect(filterText.value).toBe('');
+    //expect(filterStates.value).toEqual([]); //TODO: No funciona. cleanFilters() del component lo deja como [], pero acá sigue esperando ['DISPONIBLE', 'RESERVADO']
+
+    expect(filterLotsSpy).toHaveBeenCalled();
+
+  });
+
+  function getByTestId(testId: string) {
+    const resultHtml = fixture.debugElement.nativeElement
+    return resultHtml.querySelector(`[data-testid="${testId}"`)
+  }
+
 });
