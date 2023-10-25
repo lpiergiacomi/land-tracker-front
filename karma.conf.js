0ba804edad5bb1695684ce66a3b904848ceb1d32
Lucas Piergiacomi <lg.piergiacomi@gmail.com>
2023-10-25 14:05:07 -0300

    update karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    singleRun: false,
+    singleRun: true,
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
